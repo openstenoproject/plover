fe0113a81b9ce03fc828e0a3b401fa4f6742ddb6
Joshua Harlan Lifton <joshua.harlan.lifton@gmail.com>
2011-11-13 23:10:41 -0800

    Revamp of main GUI.
    Ubuntu's Unity window manager does not have a task bar, which leaves
    the Plover task bar icon without a home. To overcome this, the Plover
    task bar icon has been replaced with a minimal frame with the same
    functionality. This change was tested on Ubuntu 11.10. This change is
    likely a useful step toward Plover running on Mac OS X, though this
    hasn't been tested.

--- plover/gui/main.py
+++ plover/gui/main.py
@@ -11,9 +11,10 @@ configuration.
 
 import os
 import wx
+import ConfigParser
 import plover.app as app
 import plover.config as conf
-import plover.gui.config
+import plover.gui.config as gui
 import plover.exception as exception
 from plover import __name__ as __software_name__
 from plover import __version__
@@ -28,96 +29,89 @@ class PloverGUI(wx.App):
 
     def OnInit(self):
         """Called just before the application starts."""
-        steno_engine = app.StenoEngine()
-        task_bar_icon = PloverTaskBarIcon(conf.ASSETS_DIR, steno_engine)
+        frame = Frame(conf.CONFIG_FILE)
+        frame.Show()
+        self.SetTopWindow(frame)
         return True
-        
 
-class PloverTaskBarIcon(wx.TaskBarIcon):
-    """A task bar icon for controlling a Plover's steno engine.
+class Frame(wx.Frame):
+    """The top-level GUI element of the Plover application."""
 
-    When the task bar is left-clicked, Plover is toggled from paused
-    to resumed or resumed to paused. When the task bar is
-    right-clicked, a menu is displayed with About Plover, Toggle
-    Resume/Pause, and Quit options. Selecting the About Plover option
-    displays an informational dialog that can be closed by the user.
-    
-    """
     # Class constants.
-    ON_MESSAGE = "Plover is ON"
-    OFF_MESSAGE = "Plover is OFF"
+    TITLE = "Plover"
+    ALERT_DIALOG_TITLE = TITLE
     ON_IMAGE_FILE = "plover_on.png"
     OFF_IMAGE_FILE = "plover_off.png"
-    ABOUT_MENU_ITEM = "About..."
-    CONFIG_MENU_ITEM = "Configure..."
-    PAUSE_MENU_ITEM = "Pause"
-    RESUME_MENU_ITEM = "Resume"
-    QUIT_MENU_ITEM = "Quit"
-    ALERT_DIALOG_TITLE = "Plover"
+    BORDER = 5
+    RUNNING_MESSAGE = "running"
+    STOPPED_MESSAGE = "stopped"
+    CONFIGURE_BUTTON_LABEL = "Configure..."
+    ABOUT_BUTTON_LABEL = "About..."
 
-    TBMENU_CONFIG = wx.NewId()
-    TBMENU_ABOUT = wx.NewId()
-    TBMENU_PAUSE = wx.NewId()
-    TBMENU_RESUME = wx.NewId()
-    TBMENU_QUIT = wx.NewId()
+    def __init__(self, config_file):
+        wx.Frame.__init__(self, None,
+                          title=Frame.TITLE,
+                          pos=wx.DefaultPosition,
+                          size=wx.DefaultSize,
+                          style=wx.DEFAULT_FRAME_STYLE & ~(wx.RESIZE_BORDER|
+                                                           wx.RESIZE_BOX|
+                                                           wx.MAXIMIZE_BOX))
+        config_file = config_file
+        config = ConfigParser.RawConfigParser()
+        config.read(config_file)
+        self.steno_engine = app.StenoEngine()
 
-    def __init__(self, assets_dir, steno_engine):
-        """
-        
-        Arguments:
+        # Status button.
+        on_icon_file = os.path.join(conf.ASSETS_DIR, self.ON_IMAGE_FILE)
+        off_icon_file = os.path.join(conf.ASSETS_DIR, self.OFF_IMAGE_FILE)
+        self.on_bitmap = wx.Bitmap(on_icon_file, wx.BITMAP_TYPE_PNG)
+        self.off_bitmap = wx.Bitmap(off_icon_file, wx.BITMAP_TYPE_PNG)
+        self.status_button = wx.BitmapButton(self, bitmap=self.on_bitmap)
+        self.status_button.Bind(wx.EVT_BUTTON, self.OnStatusButton)
 
-        assets_dir -- The base directory containing image and other
-        assets to be used by the GUI.
+        # Configure button.
+        self.configure_button = wx.Button(self, label=self.CONFIGURE_BUTTON_LABEL)
+        self.configure_button.Bind(wx.EVT_BUTTON, self.OnConfigureButton)
 
-        steno_engine -- A StenoEngine instance to control.
+        # About button.
+        self.about_button = wx.Button(self, label=self.ABOUT_BUTTON_LABEL)
+        self.about_button.Bind(wx.EVT_BUTTON, self.OnAboutButton)
 
-        """
-        wx.TaskBarIcon.__init__(self)
-        self.steno_engine = steno_engine
+        # Layout.
+        sizer = wx.BoxSizer(wx.HORIZONTAL)
+        sizer.Add(self.status_button,
+                  flag=wx.ALL|wx.ALIGN_CENTER_VERTICAL,
+                  border=self.BORDER)
+        sizer.Add(self.configure_button,
+                  flag=wx.TOP|wx.BOTTOM|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,
+                  border=self.BORDER)
+        sizer.Add(self.about_button,
+                  flag=wx.TOP|wx.BOTTOM|wx.RIGHT|wx.ALIGN_CENTER_VERTICAL,
+                  border=self.BORDER)
+        self.SetSizer(sizer)
+        sizer.Fit(self)
 
-        # Create icons.
-        on_icon_file = os.path.join(assets_dir, self.ON_IMAGE_FILE)
-        off_icon_file = os.path.join(assets_dir, self.OFF_IMAGE_FILE)
-        self.on_icon = wx.Icon(on_icon_file, wx.BITMAP_TYPE_PNG)
-        self.off_icon = wx.Icon(off_icon_file, wx.BITMAP_TYPE_PNG)
-        self._update_icon()
-        
-        # Bind events.
-        self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.OnTaskBarActivate)
-        self.Bind(wx.EVT_MENU, self.OnTaskBarAbout, id=self.TBMENU_ABOUT)
-        self.Bind(wx.EVT_MENU, self.OnTaskBarConfig, id=self.TBMENU_CONFIG)
-        self.Bind(wx.EVT_MENU, self.OnTaskBarPause, id=self.TBMENU_PAUSE)
-        self.Bind(wx.EVT_MENU, self.OnTaskBarResume, id=self.TBMENU_RESUME)
-        self.Bind(wx.EVT_MENU, self.OnTaskBarQuit, id=self.TBMENU_QUIT)
+        self.Bind(wx.EVT_CLOSE, self.OnClose)
+        self._update_status()
 
-    def CreatePopupMenu(self):
-        """Override of the base class method.
-        
-        This method is called by the base class when it needs to popup
-        the menu for the default EVT_RIGHT_DOWN event.
+    def OnClose(self, event):
+        self.steno_engine.stop()
+        self.Destroy()
 
-        """
-        menu = wx.Menu()
-        menu.Append(self.TBMENU_ABOUT, self.ABOUT_MENU_ITEM)
-        menu.Append(self.TBMENU_CONFIG, self.CONFIG_MENU_ITEM)
-        if self.steno_engine.is_running :
-            menu.Append(self.TBMENU_PAUSE, self.PAUSE_MENU_ITEM)
-        else :
-            menu.Append(self.TBMENU_RESUME, self.RESUME_MENU_ITEM)
-        menu.AppendSeparator()
-        menu.Append(self.TBMENU_QUIT, self.QUIT_MENU_ITEM)
-        return menu
-
-    def OnTaskBarActivate(self, event):
-        """Called when the icon is left-clicked."""
+    def OnStatusButton(self, event):
+        """Called when the status button is clicked."""
         if self.steno_engine.is_running :
             self.steno_engine.stop()
         else :
             self._safe_start()
-        self._update_icon()
-        
-    def OnTaskBarAbout(self, event):
-        """Called when the About menu item is chosen."""
+        self._update_status()
+
+    def OnConfigureButton(self, event):
+        """Called when the Configure... button is clicked."""
+        self._show_config_dialog()
+
+    def OnAboutButton(self, event):
+        """Called when the About... button is clicked."""
         info = wx.AboutDialogInfo()
         info.Name = __software_name__
         info.Version = __version__
@@ -128,37 +122,13 @@ class PloverTaskBarIcon(wx.TaskBarIcon):
         info.License = __license__
         wx.AboutBox(info)
 
-    def OnTaskBarConfig(self, event):
-        """Called when the Configure menu item is chosen."""
-        self._show_config_dialog()
-
-    def OnTaskBarPause(self, event):
-        """Called when the Pause menu item is chosen."""
-        self.steno_engine.stop()
-        self._update_icon()
-        
-    def OnTaskBarResume(self, event):
-        """Called when the Resume menu item is chosen."""
-        self._safe_start()
-        self._update_icon()
-        
-    def OnTaskBarQuit(self, event):
-        """Called when the Quit menu item is chosen."""
-        self.steno_engine.stop()
-        wx.CallAfter(self.Destroy)
-
-    def _show_config_dialog(self):
-        dialog = plover.gui.config.ConfigurationDialog(conf.CONFIG_FILE)
-        dialog.Show()
-        return dialog
-
-    def _update_icon(self):
-        # Update the image used for the icon to reflect the state of
-        # the steno engine.
+    def _update_status(self):
         if self.steno_engine.is_running:
-            self.SetIcon(self.on_icon, self.ON_MESSAGE)
+            self.status_button.SetBitmapLabel(self.on_bitmap)
+            self.SetTitle("%s: %s" % (self.TITLE, self.RUNNING_MESSAGE))
         else:
-            self.SetIcon(self.off_icon, self.OFF_MESSAGE)
+            self.status_button.SetBitmapLabel(self.off_bitmap)
+            self.SetTitle("%s: %s" % (self.TITLE, self.STOPPED_MESSAGE))
 
     def _safe_start(self):
         try:
@@ -170,3 +140,8 @@ class PloverTaskBarIcon(wx.TaskBarIcon):
                                             wx.OK | wx.ICON_INFORMATION)
             alert_dialog.ShowModal()
             alert_dialog.Destroy()
+
+    def _show_config_dialog(self):
+        dialog = gui.ConfigurationDialog(conf.CONFIG_FILE)
+        dialog.Show()
+        return dialog
