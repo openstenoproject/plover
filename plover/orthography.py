3bd2fc279b5dfb4853a1d422060da2a5eeb51d6a
unknown <samlh@samlh.net>
2013-03-16 22:12:06 -0500

    Improve english suffix rules
    Rewrite suffix rules using regular expressions for readability and extensibility, and expand coverage to all suffixes

--- plover/orthography.py
+++ plover/orthography.py
@@ -3,83 +3,58 @@
 
 """Functions that implement some English orthographic rules."""
 
-CONSONANTS = 'bcdfghjklmnpqrstvwxzBCDFGHJKLMNPQRSTVWXZ'
-VOWELS = 'aeiouAEIOU'
-W = 'wW'
-Y = 'yY'
-PLURAL_SPECIAL = 'sxzSXZ'
+import re
 
-def pluralize_with_s(word):
-    """Form the plural of a word by adding an s.
+RULES = [
+    # defer + ed = deferred (consonant doubling)   XXX monitor(stress not on last syllable)
+    (re.compile(r'^(.*[bcdfghjklmnpqrstvwxyz][aeiou])([bcdfgklmnprtvz]) \^ ([aeiouy].*)$', re.I),
+        r'\1\2\2\3'),
 
-    Argument:
+    # == +ly ==
+    # artistic + ly = artistically
+    (re.compile(r'^(.*[aeiou]c) \^ ly$', re.I),
+        r'\1ally'),
 
-    word -- A singular noun or noun phrase.
+    # == y ==
+    # die+ing = dying
+    (re.compile(r'^(.+)ie \^ ing$', re.I),
+        r'\1ying'),
+    # beauty + ful = beautiful 
+    (re.compile(r'^(.+[bcdfghjklmnpqrstvwxz])y \^ ([^i].*)$', re.I),
+        r'\1i\2'),
+    # metallurgy + ist = metallurgist
+    (re.compile(r'^(.+[bcdfghjklmnpqrstvwxz])y \^ i(.*)$', re.I),
+        r'\1i\2'),
+        
+    # == e+ ==
+    # narrate + ing = narrating (silent e)
+    (re.compile(r'^(.+[bcdfghjklmnpqrstvwxz])e \^ ([aeiouy].*)$', re.I),
+        r'\1\2'),
+        
+    # == +s ==
+    # establish + s = establishes (sibilant pluralization)
+    (re.compile(r'^(.*[aeiou](?:s|x|z|ms|rs|sh|ss|tz|zz|lys|rsh|tch)|.*y(?:x|ss)) \^ s$', re.I),
+        r'\1es'),
+    # speech + s = speeches (soft ch pluralization)
+    (re.compile(r'^(.*(?:a|ee|i|oo|au|ou|r)ch) \^ s$', re.I),
+        r'\1es'),
+    # cherry + s = cherries (consonant + y pluralization)
+    (re.compile(r'^(.+[bcdfghjklmnpqrstvwxyz])y \^ s$', re.I),
+        r'\1ies')
+]
 
+def add_suffix(word, suffix):
+    """Add a suffix to a word by applying the rules above
+    
+    Arguments:
+        
+    word -- A word
+    suffix -- The suffix to add
+    
     """
-    if len(word) < 2:
-        return word + 's'
-    a = word[-2]
-    b = word[-1]
-    if b in PLURAL_SPECIAL:
-        return word + 'es'
-    elif b in Y and a in CONSONANTS:
-        return word[:-1] + 'ies'
-    return word + 's'
-
-def add_ed_suffix(word):
-    """Form the past tense of a verb by adding 'ed'.
-
-    Argument:
-
-    word -- The infinitive form of a verb.
-
-    """
-    return _prep_for_simple_suffix(word) + 'ed'
-
-def add_er_suffix(word):
-    """Add an -er suffix to the end of a word.
-
-    Argument:
-
-    word -- An adjective or verb.
-
-    """
-    return _prep_for_simple_suffix(word) + 'er'
-
-def add_ing_suffix(word):
-    """Add an -ing suffix to the end of a word.
-
-    Argument:
-
-    word -- The infinitive form of a verb.
-
-    """
-    if word and word[-1] in Y: # See _prep_for_simple_suffix special case.
-        return word + 'ing'
-    return _prep_for_simple_suffix(word) + 'ing'
-
-def _prep_for_simple_suffix(word):
-    num_chars = len(word)
-    if num_chars < 2:
-        return word
-    if num_chars >= 3:
-        third_to_last = word[-3]
-    else:
-        third_to_last = ''
-    second_to_last = word[-2]
-    last = word[-1]
-    if second_to_last in VOWELS or second_to_last in CONSONANTS:
-        if last in VOWELS:
-            if third_to_last and (third_to_last in VOWELS or
-                                  third_to_last in CONSONANTS):
-                return word[:-1]
-        elif (last in CONSONANTS and
-              last not in W and
-              second_to_last in VOWELS and
-              third_to_last and
-              third_to_last not in VOWELS):
-            return word + last
-        elif last in Y and second_to_last in CONSONANTS:
-            return word[:-1] + 'i' # Special case doesn't work for 'ing' suffix.
-    return word
+    for r in RULES:
+        m = r[0].match(word + " ^ " + suffix)
+        if m:   
+            expanded = m.expand(r[1])
+            return expanded
+    return word + suffix
