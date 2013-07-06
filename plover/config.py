1c7657c7c3f0ffac0762ec6e3ad379d8afcf0851
Hesky Fisher <hesky.fisher@gmail.com>
2013-07-06 14:48:51 -0400

    Fix default location of log file.

--- plover/config.py
+++ plover/config.py
@@ -29,7 +29,7 @@ DEFAULT_DICTIONARY_FILE = 'dict.json'
 
 LOGGING_CONFIG_SECTION = 'Logging Configuration'
 LOG_FILE_OPTION = 'log_file'
-DEFAULT_LOG_FILE = 'plover.log'
+DEFAULT_LOG_FILE = os.path.join(CONFIG_DIR, 'plover.log')
 ENABLE_STROKE_LOGGING_OPTION = 'enable_stroke_logging'
 DEFAULT_ENABLE_STROKE_LOGGING = True
 ENABLE_TRANSLATION_LOGGING_OPTION = 'enable_translation_logging'
