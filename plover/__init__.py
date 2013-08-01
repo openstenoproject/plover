f64265581df40783ebc062700f9d645ab4bd55a0
Hesky Fisher <hesky.fisher@gmail.com>
2013-08-01 15:53:33 -0400

    Update version number

--- plover/__init__.py
+++ plover/__init__.py
@@ -3,7 +3,7 @@
 
 """Plover: Open Source Stenography Software"""
 
-__version__ = '2.5.2'
+__version__ = '2.5.3'
 __copyright__ = '(C) 2010-2011 Joshua Harlan Lifton'
 __url__ = 'http://stenoknight.com/plover'
 __download_url__ = 'https://github.com/plover/plover'
