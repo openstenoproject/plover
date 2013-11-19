1f3b2a841776260c99b16d90b99e49412a4f8313
Hesky Fisher <hesky.fisher@gmail.com>
2013-11-19 11:50:49 -0500

    work in progress

---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    // Choose the default tab based on the user's OS.
+    var os='windows';
+    if (navigator.platform.toUpperCase().indexOf('MAC')!==-1) {
+        os = 'osx';
+    } else if (navigator.platform.toUpperCase().indexOf('LINUX')!==-1) {
+        os = 'linux';
+    }
+    $('#os_tab a[href="#' + os + '"]').tab('show')
+});
