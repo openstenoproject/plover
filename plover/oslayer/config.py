0ef36f8b21ec8a6fa0ad57971f36b6f5a4f03ac7
Benoit Pierre <benoit.pierre@gmail.com>
2015-10-24 23:51:46 +0200

    use pkg_resources for assets when available
    Note: this is disabled when run from a Mac OS X bundle,
    since it's unsupported by py2app.

--- plover/oslayer/config.py
+++ plover/oslayer/config.py
@@ -3,23 +3,30 @@
 
 """Platform dependent configuration."""
 
+import pkg_resources
 import appdirs
 import os
 from os.path import realpath, join, dirname, abspath, isfile, pardir
 import sys
 
+bundle_type = None
 
 # If plover is run from a pyinstaller binary.
 if hasattr(sys, 'frozen') and hasattr(sys, '_MEIPASS'):
     ASSETS_DIR = sys._MEIPASS
     PROGRAM_DIR = dirname(sys.executable)
+    bundle_type = 'pyinstaller'
 # If plover is run from an app bundle on Mac.
 elif (sys.platform.startswith('darwin') and '.app' in realpath(__file__)):
     ASSETS_DIR = os.getcwd()
     PROGRAM_DIR = abspath(join(dirname(sys.executable), *[pardir] * 3))
+    bundle_type = 'mac'
 else:
     ASSETS_DIR = join(dirname(dirname(realpath(__file__))), 'assets')
     PROGRAM_DIR = os.getcwd()
+
+if bundle_type != 'mac' and pkg_resources.resource_isdir('plover', 'assets'):
+    ASSETS_DIR = pkg_resources.resource_filename('plover', 'assets')
 
 # If the program's directory has a plover.cfg file then run in "portable mode",
 # i.e. store all data in the same directory. This allows keeping all Plover
