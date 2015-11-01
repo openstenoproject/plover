c657a4b38a642a8a8a8cf8f106d3c6f4415997ad
Benoit Pierre <benoit.pierre@gmail.com>
2015-11-01 17:22:48 +0100

    oslayer: improve keyboard test code
    Suppress all supported keys.

--- plover/oslayer/keyboardcontrol.py
+++ plover/oslayer/keyboardcontrol.py
@@ -44,7 +44,11 @@ class KeyboardEmulation(keyboardcontrol.KeyboardEmulation):
 if __name__ == '__main__':
     import time
 
-    keys = [chr(n) for n in range(ord('a'), ord('z'))]
+    keys = [chr(n) for n in range(ord('a'), ord('z') + 1)]
+    for n in range(12):
+        keys.append('F%u' % (n + 1))
+    for c in '`1234567890-=[];\',./\\':
+        keys.append(c)
     keys.append('space')
     kc = KeyboardCapture(keys)
     ke = KeyboardEmulation()
