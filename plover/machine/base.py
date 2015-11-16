e8ff262d16d2d8820106e539cb59f3fba1f1d98c
Benoit Pierre <benoit.pierre@gmail.com>
2015-11-16 03:32:01 +0100

    machine: improved logging

--- plover/machine/base.py
+++ plover/machine/base.py
@@ -9,6 +9,7 @@
 import serial
 import threading
 import collections
+from plover import log
 
 STATE_STOPPED = 'closed'
 STATE_INITIALIZING = 'initializing'
@@ -157,7 +158,7 @@ class SerialStenotypeBase(ThreadedStenotypeBase):
         try:
             self.serial_port = serial.Serial(**self.serial_params)
         except (serial.SerialException, OSError) as e:
-            print e
+            log.error('Opening serial port: %s', str(e))
             self._error()
             return
         if self.serial_port is None or not self.serial_port.isOpen():
