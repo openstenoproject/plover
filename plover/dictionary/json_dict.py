1ea0fc76d5dcfc212feba4244715b16f0b2db5ea
Benoit Pierre <benoit.pierre@gmail.com>
2015-11-16 03:31:24 +0100

    dictionary: more informative exceptions

--- plover/dictionary/json_dict.py
+++ plover/dictionary/json_dict.py
@@ -15,19 +15,20 @@ except ImportError:
     import json
     
 
-def load_dictionary(data):
+def load_dictionary(fp):
     """Load a json dictionary from a string."""
 
     def h(pairs):
         return StenoDictionary((normalize_steno(x[0]), x[1]) for x in pairs)
 
+    data = fp.read()
     try:
         try:
             return json.loads(data, object_pairs_hook=h)
         except UnicodeDecodeError:
             return json.loads(data, 'latin-1', object_pairs_hook=h)
-    except ValueError:
-        raise DictionaryLoaderException('Dictionary is not valid json.')
+    except ValueError as e:
+        raise DictionaryLoaderException('\'%s\' is not valid json: %s' % (fp.name, str(e)))
         
 # TODO: test this
 def save_dictionary(d, fp):
