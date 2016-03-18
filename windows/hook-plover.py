13a92126b9eca0ce0eceaaf188d9561d4957883f
Benoit Pierre <benoit.pierre@gmail.com>
2016-03-18 02:30:57 +0100

    windows: update PyInstaller hook so pkg_resources can be used

--- windows/hook-plover.py
+++ windows/hook-plover.py
@@ -1,6 +1,23 @@
 
+import pkg_resources
+
+from utils.metadata import collect_metadata
+
+from PyInstaller import log as logging
+
+
+log = logging.getLogger(__name__)
+log.info('hook-plover.py')
+
 datas = []
 hiddenimports = []
 
-datas.append(('plover/assets/*', '.'))
+distribution = list(pkg_resources.find_distributions('.', only=True))[0]
+assert distribution.project_name == 'Plover'
+
+metadata_list = collect_metadata(distribution)
+log.info('adding metadata: %s', metadata_list)
+datas.extend(metadata_list)
+
+datas.append(('plover/assets', 'plover/assets'))
 
