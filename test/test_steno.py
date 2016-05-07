276c430adc0601516df02719831f389e15962631
Benoit Pierre <benoit.pierre@gmail.com>
2016-05-08 00:12:08 +0200

    steno: improve normalize_steno tests
    - better assert message
    - more tests

--- test/test_steno.py
+++ test/test_steno.py
@@ -11,33 +11,47 @@ from plover.steno import normalize_steno, Stroke
 class StenoTestCase(unittest.TestCase):
     def test_normalize_steno(self):
         cases = (
-        
-        # TODO: More cases
-        ('S', 'S'),
-        ('S-', 'S'),
-        ('-S', '-S'),
-        ('ES', 'ES'),
-        ('-ES', 'ES'),
-        ('TW-EPBL', 'TWEPBL'),
-        ('TWEPBL', 'TWEPBL'),
-        ('19', '1-9'),
-        ('14', '14'),
-        ('146', '14-6'),
-        ('67', '-67'),
-        ('6', '-6'),
-        ('9', '-9'),
-        ('5', '5'),
-        ('0', '0'),
-        ('456', '456'),
-        ('46', '4-6'),
-        ('4*6', '4*6'),
-        ('456', '456'),
-        ('S46', 'S4-6'),
+            # TODO: More cases
+            ('S', 'S'),
+            ('S-', 'S'),
+            ('-S', '-S'),
+            ('ES', 'ES'),
+            ('-ES', 'ES'),
+            ('TW-EPBL', 'TWEPBL'),
+            ('TWEPBL', 'TWEPBL'),
+            ('19', '1-9'),
+            ('14', '14'),
+            ('146', '14-6'),
+            ('67', '-67'),
+            ('6', '-6'),
+            ('9', '-9'),
+            ('5', '5'),
+            ('0', '0'),
+            ('456', '456'),
+            ('46', '4-6'),
+            ('4*6', '4*6'),
+            ('456', '456'),
+            ('S46', 'S4-6'),
+            # Number key.
+            ('#S', '#S'),
+            ('#A', '#A'),
+            ('#0', '0'),
+            ('#6', '-6'),
+            # Implicit hyphens.
+            ('SA-', 'SA'),
+            ('SA-R', 'SAR'),
+            ('-O', 'O'),
+            ('S*-R', 'S*R'),
+            ('S-*R', 'S*R'),
         )
-        
+
         for arg, expected in cases:
-            self.assertEqual('/'.join(normalize_steno(arg)), expected)
-            
+            result = '/'.join(normalize_steno(arg))
+            msg = 'normalize_steno(%r)=%r != %r' % (
+                arg, result, expected,
+            )
+            self.assertEqual(result, expected, msg=msg)
+
     def test_steno(self):
         self.assertEqual(Stroke(['S-']).rtfcre, 'S')
         self.assertEqual(Stroke(['S-', 'T-']).rtfcre, 'ST')
