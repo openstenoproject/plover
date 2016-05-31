c8557bd9b85169364d33f08a7be122e3fac290ae
Benoit Pierre <benoit.pierre@gmail.com>
2016-05-31 15:35:31 +0200

    oslayer/osxkeyboardcontrol: minor cleanup

--- plover/oslayer/osxkeyboardcontrol.py
+++ plover/oslayer/osxkeyboardcontrol.py
@@ -124,32 +124,6 @@ def up(seq):
 def down_up(seq):
     return down(seq) + up(seq)
 
-# Maps from literal characters to their key names.
-LITERALS = collections.defaultdict(str, {
-    '0': '0', '1': '1', '2': '2', '3': '3', '4': '4', '5': '5', '6': '6',
-    '7': '7', '8': '8', '9': '9',
-
-    'a': 'a', 'b': 'b', 'c': 'c', 'd': 'd', 'e': 'e', 'f': 'f', 'g': 'g',
-    'h': 'h', 'i': 'i', 'j': 'j', 'k': 'k', 'l': 'l', 'm': 'm', 'n': 'n',
-    'o': 'o', 'p': 'p', 'q': 'q', 'r': 'r', 's': 's', 't': 't', 'u': 'u',
-    'v': 'v', 'w': 'w', 'x': 'x', 'y': 'y', 'z': 'z',
-
-    'A': 'A', 'B': 'B', 'C': 'C', 'D': 'D', 'E': 'E', 'F': 'F', 'G': 'G',
-    'H': 'H', 'I': 'I', 'J': 'J', 'K': 'K', 'L': 'L', 'M': 'M', 'N': 'N',
-    'O': 'O', 'P': 'P', 'Q': 'Q', 'R': 'R', 'S': 'S', 'T': 'T', 'U': 'U',
-    'V': 'V', 'W': 'W', 'X': 'X', 'Y': 'Y', 'Z': 'Z',
-
-    '`': 'grave', '~': 'asciitilde', '!': 'exclam', '@': 'at',
-    '#': 'numbersign', '$': 'dollar', '%': 'percent', '^': 'asciicircum',
-    '&': 'ampersand', '*': 'asterisk', '(': 'parenleft', ')': 'parenright',
-    '-': 'minus', '_': 'underscore', '=': 'equal', '+': 'plus',
-    '[': 'bracketleft', ']': 'bracketright', '{': 'braceleft',
-    '}': 'braceright', '\\': 'backslash', '|': 'bar', ';': 'semicolon',
-    ':': 'colon', '\'': 'apostrophe', '"': 'quotedbl', ',': 'comma',
-    '<': 'less', '.': 'period', '>': 'greater', '/': 'slash',
-    '?': 'question', '\t': 'Tab', ' ': 'space'
-})
-
 # Maps from keycodes to corresponding event masks.
 MODIFIER_KEYS_TO_MASKS = {
     58: kCGEventFlagMaskAlternate,
