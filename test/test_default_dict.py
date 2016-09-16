3ea7b61d5ea1cc73328e2a4921b5681a3c2a5ca1
Benoit Pierre <benoit.pierre@gmail.com>
2016-09-16 21:29:38 +0200

    test: improve default dictionaries tests
    Detect shadowed/overridden keys in lower precedence dictionaries.

--- test/test_default_dict.py
+++ test/test_default_dict.py
@@ -4,6 +4,7 @@
 import io
 import json
 import unittest
+from collections import defaultdict
 
 
 DICT_NAMES = ['main.json',
@@ -12,26 +13,18 @@ DICT_NAMES = ['main.json',
 DICT_PATH = 'plover/assets/'
 
 
-def read_key_pairs(pairs):
-    d = {}
-    for key, value in pairs:
-        holder = []
-        if key in d:
-            holder = d[key]
-        else:
-            d[key] = holder
-        holder.append(value)
-    return d
-
-
 class TestCase(unittest.TestCase):
 
     def test_no_duplicates_categorized_files(self):
-        d = {}
+        d = defaultdict(list)
+        dictionary = None
+        def read_key_pairs(pairs):
+            for key, value in pairs:
+                d[key].append((value, dictionary))
+            return d
         for dictionary in map(lambda x: DICT_PATH + x, DICT_NAMES):
-            d.update(json.load(io.open(dictionary, encoding='utf-8'),
-                               object_pairs_hook=read_key_pairs))
-
+            json.load(io.open(dictionary, encoding='utf-8'),
+                      object_pairs_hook=read_key_pairs)
         msg_list = []
         has_duplicate = False
         for key, value_list in d.items():
@@ -39,6 +32,6 @@ class TestCase(unittest.TestCase):
                 has_duplicate = True
                 msg_list.append('key: %s\n' % key)
                 for value in value_list:
-                    msg_list.append('%s\n' % value)
-        msg = ''.join(msg_list)
+                    msg_list.append('%r in %s\n' % value)
+        msg = '\n' + ''.join(msg_list)
         self.assertFalse(has_duplicate, msg)
