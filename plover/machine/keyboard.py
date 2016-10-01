1752263e298449dbaffae6adebadb67478a631c5
KeyboardFire <andy@keyboardfire.com>
2016-10-01 15:27:17 -0500

    Implement arpeggiate_group config option
    This is the first step of implementing #591. The arpeggiate_group option
    is an array listed under [Keyboard] in plover.cfg, which contains a list
    of keys that should trigger arpeggiation.
    
    Currently, the option works when an arpeggiation key is the last key
    pressed in a chord, but it does not work in the middle of a chord.
    
    The option is not yet exposed in the configuration GUI.

--- plover/machine/keyboard.py
+++ plover/machine/keyboard.py
@@ -23,6 +23,7 @@ class Keyboard(StenotypeBase):
         """Monitor the keyboard's events."""
         super(Keyboard, self).__init__()
         self._arpeggiate = params['arpeggiate']
+        self._arpeggiate_group = params['arpeggiate_group']
         self._is_suppressed = False
         self._bindings = {}
         self._down_keys = set()
@@ -44,9 +45,10 @@ class Keyboard(StenotypeBase):
             if 'no-op' == mapping:
                 self._bindings[key] = None
             elif 'arpeggiate' == mapping:
+                # The "arpeggiate" mapping acts like a no-op when in arpeggiate
+                # mode, but passes the key through unchanged otherwise.
                 if self._arpeggiate:
                     self._bindings[key] = None
-                    self._arpeggiate_key = key
                 else:
                     # Don't suppress arpeggiate key if it's not used.
                     del self._bindings[key]
@@ -113,7 +115,7 @@ class Keyboard(StenotypeBase):
         send_strokes = bool(self._down_keys and
                             self._down_keys == self._released_keys)
         if self._arpeggiate:
-            send_strokes &= key == self._arpeggiate_key
+            send_strokes &= key in self._arpeggiate_group
         if send_strokes:
             self._last_stroke_key_down_count = self._stroke_key_down_count
             steno_keys = list(self._down_keys)
@@ -127,4 +129,6 @@ class Keyboard(StenotypeBase):
         bool_converter = lambda s: s == 'True'
         return {
             'arpeggiate': (False, bool_converter),
+            # TODO don't use eval
+            'arpeggiate_group': (['space'], lambda s: eval(s)),
         }
