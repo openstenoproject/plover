a2393dd78a22dc760b7c65d09103e167ea196e05
Benoit Pierre <benoit.pierre@gmail.com>
2017-03-15 10:10:13 +0100

    fix launch.sh to work on macOS
    No `readlink -e` on macOS...

--- launch.sh
+++ launch.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
-cd "$(dirname "$(readlink -e "$0")")" &&
-exec ./setup.py launch -- "$@"
+set -e
+
+# No `readlink -e` on macOS...
+srcdir="$(python3 - "$0" <<\EOF
+import os
+import sys
+
+print(os.path.dirname(os.path.realpath(sys.argv[1])))
+EOF
+)"
+
+cd "$srcdir"
+exec python3 setup.py launch -- "$@"
