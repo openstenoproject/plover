5a69da33d69ae3f3089302d4f7df7b9d40484c2f
Benoit Pierre <benoit.pierre@gmail.com>
2017-03-23 23:24:31 +0100

    resource: better handling of invalid asset specifications

--- plover/resource.py
+++ plover/resource.py
@@ -7,7 +7,10 @@ import pkg_resources
 ASSET_SCHEME = 'asset:'
 
 def _asset_split(resource_name):
-    return resource_name[len(ASSET_SCHEME):].split(':', 2)
+    components = resource_name[len(ASSET_SCHEME):].split(':', 2)
+    if len(components) != 2:
+        raise ValueError('invalid asset: %s' % resource_name)
+    return components
 
 def resource_exists(resource_name):
     if resource_name.startswith(ASSET_SCHEME):
