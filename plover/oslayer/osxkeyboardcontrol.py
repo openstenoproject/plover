4b21d11668b944e4495bb18309dffc24b9435377
Ted Morin <morinted@gmail.com>
2017-03-24 10:36:20 -0400

    osxkeyboardcontrol: also add SecondaryFn mask to Command key

--- plover/oslayer/osxkeyboardcontrol.py
+++ plover/oslayer/osxkeyboardcontrol.py
@@ -127,7 +127,7 @@ MODIFIER_KEYS_TO_MASKS = {
     62: kCGEventFlagMaskControl | kCGEventFlagMaskSecondaryFn,
     56: kCGEventFlagMaskShift,
     60: kCGEventFlagMaskShift,
-    55: kCGEventFlagMaskCommand,
+    55: kCGEventFlagMaskCommand | kCGEventFlagMaskSecondaryFn,
     63: kCGEventFlagMaskSecondaryFn,
 }
 
