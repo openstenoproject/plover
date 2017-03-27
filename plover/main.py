b91cc79f8286b9e5b7e8c3bf80646922d238cf5c
Benoit Pierre <benoit.pierre@gmail.com>
2017-03-28 01:24:02 +0200

    main: add support for running console scripts

--- plover/main.py
+++ plover/main.py
@@ -12,6 +12,8 @@ import sys
 import traceback
 import argparse
 
+import pkg_resources
+
 if sys.platform.startswith('darwin'):
     import appnope
 
@@ -45,6 +47,10 @@ def main():
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('--version', action='version', version='%s %s'
                         % (__software_name__.capitalize(), __version__))
+    parser.add_argument('-s', '--script', default=None, nargs=argparse.REMAINDER,
+                        help='use another plugin console script as main entrypoint, '
+                        'passing in the rest of the command line arguments, '
+                        'print list of available scripts when no argument is given')
     parser.add_argument('-l', '--log-level', choices=['debug', 'info', 'warning', 'error'],
                         default=None, help='set log level')
     parser.add_argument('-g', '--gui', default='qt', help='set gui')
@@ -59,6 +65,46 @@ def main():
     gui = registry.get_plugin('gui', args.gui).obj
 
     try:
+        if args.script is not None:
+            if args.script:
+                # Create a mapping of available console script,
+                # with the following priorities (highest first):
+                # - {project_name}-{version}:{script_name}
+                # - {project_name}:{script_name}
+                # - {script_name}
+                console_scripts = {}
+                for e in sorted(pkg_resources.iter_entry_points('console_scripts'),
+                                key=lambda e: (e.dist, e.name)):
+                    for key in (
+                        '%s-%s:%s' % (e.dist.project_name, e.dist.version, e.name),
+                        '%s:%s' % (e.dist.project_name, e.name),
+                        e.name,
+                    ):
+                        console_scripts[key] = e
+                entrypoint = console_scripts.get(args.script[0])
+                if entrypoint is None:
+                    log.error('no such script: %s', args.script[0])
+                    code = 1
+                else:
+                    sys.argv = args.script
+                    try:
+                        code = entrypoint.load()()
+                    except SystemExit as e:
+                        code = e.code
+                if code is None:
+                    code = 0
+            else:
+                print('available script(s):')
+                dist = None
+                for e in sorted(pkg_resources.iter_entry_points('console_scripts'),
+                                key=lambda e: (str(e.dist), e.name)):
+                    if dist != e.dist:
+                        dist = e.dist
+                        print('%s:' % dist)
+                    print('- %s' % e.name)
+                code = 0
+            os._exit(code)
+
         # Ensure only one instance of Plover is running at a time.
         with plover.oslayer.processlock.PloverLock():
             if sys.platform.startswith('darwin'):
