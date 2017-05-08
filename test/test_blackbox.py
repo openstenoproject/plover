9f017e9c1d99ae7876e73b0b9a966aca7b8a76b1
Benoit Pierre <benoit.pierre@gmail.com>
2017-05-08 22:32:58 +0200

    test: rework blackbox tests to make it easier to add new ones

--- test/test_blackbox.py
+++ test/test_blackbox.py
@@ -1,6 +1,9 @@
 
+import ast
+import functools
 import unittest
 
+from plover import system
 from plover.steno import Stroke
 from plover.formatting import Formatter
 from plover.translation import Translator
@@ -31,10 +34,61 @@ class CaptureOutput(object):
 
 
 def steno_to_stroke(steno):
-    stroke = Stroke(())
-    stroke.rtfcre = steno
-    stroke.is_correction = steno == '*'
-    return stroke
+    if steno_to_stroke.system != system.NAME:
+        keys = []
+        letters = ''
+        has_hyphen = False
+        for k in system.KEYS:
+            if not has_hyphen and k.startswith('-'):
+                has_hyphen = True
+                keys.append(None)
+                letters += '-'
+            keys.append(k)
+            letters += k.strip('-')
+        steno_to_stroke.keys = keys
+        steno_to_stroke.letters = letters
+        steno_to_stroke.system = system.NAME
+        steno_to_stroke.numbers = {
+            v.strip('-'): k.strip('-')
+            for k, v in system.NUMBERS.items()
+        }
+    n = -1
+    keys = set()
+    for l in steno:
+        rl = steno_to_stroke.numbers.get(l)
+        if rl is not None:
+            keys.add('#')
+            l = rl
+        n = steno_to_stroke.letters.find(l, n + 1)
+        assert n >= 0, (steno_to_stroke.letters, l, n)
+        k = steno_to_stroke.keys[n]
+        if k is not None:
+            keys.add(k)
+    return Stroke(keys)
+
+steno_to_stroke.system = None
+
+
+def simple_replay(f):
+    @functools.wraps(f)
+    def wrapper(self):
+        f(self)
+        definitions, strokes, output = f.__doc__.strip().split('\n\n')
+        for steno, translation in ast.literal_eval(
+            '{' + definitions + '}'
+        ).items():
+            self.dictionary.set(normalize_steno(steno), translation)
+        for s in normalize_steno(strokes.strip()):
+            self.translator.translate(steno_to_stroke(s))
+        self.assertEqual(self.output.text, ast.literal_eval(output.strip()), msg=f.__doc__)
+    return wrapper
+
+def spaces_after(f):
+    @functools.wraps(f)
+    def wrapper(self):
+        self.formatter.set_space_placement('After Output')
+        f(self)
+    return wrapper
 
 
 class BlackboxTest(unittest.TestCase):
@@ -49,209 +103,182 @@ class BlackboxTest(unittest.TestCase):
         self.dictionary = self.translator.get_dictionary()
         self.dictionary.set_dicts([StenoDictionary()])
 
+    @simple_replay
     def test_translator_state_handling(self):
         # Check if the translator curtailing the list of last translations
         # according to its dictionary longest key does no affect things
         # like the restrospective repeate-last-stroke command.
-        self.dictionary.set(('TEFT',), 'test')
-        self.dictionary.set(('R*S',), '{*+}')
-        # Note: the implementation of repeat-last-stroke looks at the last
-        # stroke keys, so we can't use the same trick as for other tests.
-        for keys in (
-            ('T-', '-E', '-F', '-T'),
-            ('R-', '*', '-S'),
-        ):
-            stroke = Stroke(keys)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u' test test')
+        r'''
+        "TEFT": "test",
+        "R*S": "{*+}",
 
+        TEFT/R*S
+
+        " test test"
+        '''
+
+    @simple_replay
     def test_force_lowercase_title(self):
-        self.dictionary.set(('T-LT',), '{MODE:TITLE}')
-        self.dictionary.set(('TEFT',), '{>}test')
-        for keys in (
-            ('T-', '-L', '-T'),
-            ('T-', '-E', '-F', '-T'),
-        ):
-            stroke = Stroke(keys)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u' test')
+        r'''
+        "T-LT": "{MODE:TITLE}",
+        "TEFT": "{>}test",
 
+        T-LT/TEFT
+
+        " test"
+        '''
+
+    @simple_replay
     def test_bug471(self):
         # Repeat-last-stroke after typing two numbers outputs the numbers
         # reversed for some combos.
-        self.dictionary.set(('R*S',), '{*+}')
-        # Note: the implementation of repeat-last-stroke looks at the last
-        # stroke keys, so we can't use the same trick as for other tests.
-        for keys in (
-            ('#', 'S-', 'T-'), # 12
-            ('R-', '*', '-S'),
-        ):
-            stroke = Stroke(keys)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u' 1212')
+        r'''
+        "R*S": "{*+}",
 
+        12/R*S
+
+        " 1212"
+        '''
+
+    @simple_replay
     def test_bug535(self):
         # Currency formatting a number with a decimal fails by not erasing
         # the previous output.
-        self.dictionary.set(('P-P',), '{^.^}')
-        self.dictionary.set(('KR*UR',), '{*($c)}')
-        for steno in (
-            '1',
-            'P-P',
-            '2',
-            'KR*UR',
-        ):
-            stroke = steno_to_stroke(steno)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u' $1.20')
+        r'''
+        "P-P": "{^.^}",
+        "KR*UR": "{*($c)}",
 
+        1/P-P/2/KR*UR
+
+        " $1.20"
+        '''
+
+    @simple_replay
+    @spaces_after
     def test_bug606(self):
-        for steno, translation in (
-            ('KWEGS', 'question'),
-            ('-S'   , '{^s}'    ),
-            ('TP-PL', '{.}'     ),
-        ):
-            self.dictionary.set(normalize_steno(steno), translation)
-        self.formatter.set_space_placement('After Output')
-        for steno in (
-            'KWEGS',
-            '-S',
-            'TP-PL',
-        ):
-            stroke = steno_to_stroke(steno)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u'questions. ')
+        r'''
+        "KWEGS": "question",
+        "-S": "{^s}",
+        "TP-PL": "{.}",
 
+        KWEGS/-S/TP-PL
+
+        "questions. "
+        '''
+
+    @simple_replay
+    @spaces_after
     def test_bug535_spaces_after(self):
         # Currency formatting a number with a decimal fails by not erasing
         # the previous output.
-        self.formatter.set_space_placement('After Output')
-        self.dictionary.set(('P-P',), '{^.^}')
-        self.dictionary.set(('KR*UR',), '{*($c)}')
-        for steno in (
-            '1',
-            'P-P',
-            '2',
-            'KR*UR',
-        ):
-            stroke = steno_to_stroke(steno)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u'$1.20 ')
+        r'''
+        "P-P": "{^.^}",
+        "KR*UR": "{*($c)}",
 
+        1/P-P/2/KR*UR
+
+        "$1.20 "
+        '''
+
+    @simple_replay
+    @spaces_after
     def test_bug557(self):
         # Using the asterisk key to delete letters in fingerspelled words
         # occasionally causes problems when the space placement is set to
         # "After Output".
-        for steno, translation in (
-            ('EU'      , 'I'      ),
-            ('HRAOEUBG', 'like'   ),
-            ('T*'      , '{>}{&t}'),
-            ('A*'      , '{>}{&a}'),
-            ('KR*'     , '{>}{&c}'),
-            ('O*'      , '{>}{&o}'),
-            ('S*'      , '{>}{&s}'),
-        ):
-            self.dictionary.set(normalize_steno(steno), translation)
-        self.formatter.set_space_placement('After Output')
-        for steno in (
-            'EU',
-            'HRAOEUBG',
-            'T*', 'A*', 'KR*', 'O*', 'S*',
-                          '*',  '*',  '*',
-        ):
-            stroke = steno_to_stroke(steno)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u'I like ta ')
+        r'''
+        "EU": "I",
+        "HRAOEUBG": "like",
+        "T*": "{>}{&t}",
+        "A*": "{>}{&a}",
+        "KR*": "{>}{&c}",
+        "O*": "{>}{&o}",
+        "S*": "{>}{&s}",
 
+        EU/HRAOEUBG/T*/A*/KR*/O*/S*/*/*/*
+
+        "I like ta "
+        '''
+
+    @simple_replay
+    @spaces_after
     def test_bug557_resumed(self):
         # Using the asterisk key to delete letters in fingerspelled words
         # occasionally causes problems when the space placement is set to
         # "After Output".
-        for steno, translation in (
-            ('EU'      , 'I'      ),
-            ('HRAOEUBG', 'like'   ),
-            ('T*'      , '{>}{&t}'),
-            ('A*'      , '{>}{&a}'),
-            ('KR*'     , '{>}{&c}'),
-            ('O*'      , '{>}{&o}'),
-            ('S*'      , '{>}{&s}'),
-        ):
-            self.dictionary.set(normalize_steno(steno), translation)
-        self.formatter.set_space_placement('After Output')
-        for steno in (
-            'EU',
-            'HRAOEUBG',
-            'T*', 'A*', 'KR*', 'O*', 'S*',
-            '*', '*', '*', '*', '*',
-            'HRAOEUBG',
-        ):
-            stroke = steno_to_stroke(steno)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u'I like like ')
+        r'''
+        "EU": "I",
+        "HRAOEUBG": "like",
+        "T*": "{>}{&t}",
+        "A*": "{>}{&a}",
+        "KR*": "{>}{&c}",
+        "O*": "{>}{&o}",
+        "S*": "{>}{&s}",
 
+        EU/HRAOEUBG/T*/A*/KR*/O*/S*/*/*/*/*/*/HRAOEUBG
+
+        "I like like "
+        '''
+
+    @simple_replay
+    @spaces_after
     def test_bug557_capitalized(self):
         # Using the asterisk key to delete letters in fingerspelled words
         # occasionally causes problems when the space placement is set to
         # "After Output".
-        for steno, translation in (
-            ('EU'      , 'I'      ),
-            ('HRAOEUBG', 'like'   ),
-            ('T*'      , '{-|}{&t}'),
-            ('A*'      , '{-|}{&a}'),
-            ('KR*'     , '{-|}{&c}'),
-            ('O*'      , '{-|}{&o}'),
-            ('S*'      , '{-|}{&s}'),
-        ):
-            self.dictionary.set(normalize_steno(steno), translation)
-        self.formatter.set_space_placement('After Output')
-        for steno in (
-            'EU',
-            'HRAOEUBG',
-            'T*', 'A*', 'KR*', 'O*', 'S*',
-                          '*',  '*',  '*',
-        ):
-            stroke = steno_to_stroke(steno)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u'I like TA ')
+        r'''
+        "EU": "I",
+        "HRAOEUBG": "like",
+        "T*": "{-|}{&t}",
+        "A*": "{-|}{&a}",
+        "KR*": "{-|}{&c}",
+        "O*": "{-|}{&o}",
+        "S*": "{-|}{&s}",
 
+        EU/HRAOEUBG/T*/A*/KR*/O*/S*/*/*/*
+
+        "I like TA "
+        '''
+
+    @simple_replay
+    @spaces_after
     def test_capitalized_fingerspelling_spaces_after(self):
         # Using the asterisk key to delete letters in fingerspelled words
         # occasionally causes problems when the space placement is set to
         # "After Output".
-        for steno, translation in (
-            ('HRAOEUBG', 'like'   ),
-            ('T*'      , '{&T}'),
-            ('A*'      , '{&A}'),
-            ('KR*'     , '{&C}'),
-            ('O*'      , '{&O}'),
-            ('S*'      , '{&S}'),
-        ):
-            self.dictionary.set(normalize_steno(steno), translation)
-        self.formatter.set_space_placement('After Output')
-        for steno in (
-            'HRAOEUBG',
-            'T*', 'A*', 'KR*', 'O*', 'S*',
-        ):
-            stroke = steno_to_stroke(steno)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u'like TACOS ')
+        r'''
+        "HRAOEUBG": "like",
+        "T*": "{&T}",
+        "A*": "{&A}",
+        "KR*": "{&C}",
+        "O*": "{&O}",
+        "S*": "{&S}",
 
+        HRAOEUBG/T*/A*/KR*/O*/S*
+
+        "like TACOS "
+        '''
+
+    @simple_replay
     def test_special_characters(self):
-        self.dictionary.set(('R-R',), '{^}\n{^}')
-        self.dictionary.set(('TAB',), '\t')
-        for steno in (
-            'R-R',
-            'TAB',
-        ):
-            stroke = steno_to_stroke(steno)
-            self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u'\n\t')
+        r'''
+        "R-R": "{^}\n{^}",
+        "TAB": "\t",
 
+        R-R/TAB
+
+        "\n\t"
+        '''
+
+    @simple_replay
     def test_automatic_suffix_keys_1(self):
-        self.dictionary.set(('RAEUS',), 'race')
-        self.dictionary.set(('RAEUZ',), 'raise')
-        self.dictionary.set(('-S',), '{^s}')
-        self.dictionary.set(('-Z',), '{^s}')
-        stroke = Stroke(('R-', 'A-', '-E', '-U', '-S', '-Z'))
-        self.translator.translate(stroke)
-        self.assertEqual(self.output.text, u' races')
+        r'''
+        "RAEUS": "race",
+        "RAEUZ": "raise",
+        "-S": "{^s}",
+        "-Z": "{^s}",
+
+        RAEUSZ
+
+        " races"
+        '''
