c23442af8ca689f5a6bf1b0d8b43683746906db0
Benoit Pierre <benoit.pierre@gmail.com>
2018-03-09 15:20:56 +0100

    tests: rework passport tests to use pytest

--- test/test_passport.py
+++ test/test_passport.py
@@ -3,70 +3,60 @@
 
 """Unit tests for passport.py."""
 
-import time
-import unittest
-
-from mock import patch
+import threading
 
 from plover.machine.passport import Passport
+
+from . import parametrize
 
 
 class MockSerial:
 
-    inputs = []
-    index = 0
-
     def __init__(self, **params):
-        MockSerial.index = 0
+        pass
 
     def isOpen(self):
         return True
 
-    def _get(self):
-        if len(MockSerial.inputs) > MockSerial.index:
-            return MockSerial.inputs[MockSerial.index]
-        return ''
-
     def inWaiting(self):
-        return len(self._get())
+        return len(self.data)
 
     def read(self, size=1):
-        assert size == self.inWaiting()
-        result = self._get()
-        MockSerial.index += 1
-        return result
-        
+        assert size >= self.inWaiting()
+        if not self.data:
+            return ''
+        data = self.data.pop(0)
+        if not self.data:
+            self.event.set()
+        return data
+
     def close(self):
         pass
 
 
-class TestCase(unittest.TestCase):
-    def test_passport(self):
-        
-        def p(s):
-            return b'<123/' + s + b'/something>'
-        
-        cases = (
-            # Test all keys
-            ((b'!f#f+f*fAfCfBfEfDfGfFfHfKfLfOfNfQfPfSfRfUfTfWfYfXfZf^f~f',),
-            [set(Passport.get_keys()),]),
-            # Anything below 8 is not registered
-            ((b'S9T8A7',), [['S', 'T'],]),
-            # Sequence of strokes
-            ((b'SfTf', b'Zf', b'QfLf'), [['S', 'T'], ['Z',], ['Q', 'L']]),
-        )
+PASSPORT_TESTS = (
+    # Test all keys
+    lambda: ((b'!f#f+f*fAfCfBfEfDfGfFfHfKfLfOfNfQfPfSfRfUfTfWfYfXfZf^f~f',),
+             [set(Passport.get_keys()),]),
+    # Anything below 8 is not registered
+    lambda: ((b'S9T8A7',), [['S', 'T'],]),
+    # Sequence of strokes
+    lambda: ((b'SfTf', b'Zf', b'QfLf'), [['S', 'T'], ['Z',], ['Q', 'L']]),
+)
 
-        params = {k: v[0] for k, v in Passport.get_option_info().items()}
-        with patch('plover.machine.base.serial.Serial', MockSerial) as mock:
-            for inputs, expected in cases:
-                mock.inputs = list(map(p, inputs))
-                actual = []
-                m = Passport(params)
-                m.add_stroke_callback(actual.append)
-                m.start_capture()
-                while mock.index < len(mock.inputs):
-                    time.sleep(0.00001)
-                m.stop_capture()
-                self.assertEqual(len(actual), len(expected))
-                for actual_keys, expected_keys in zip(actual, expected):
-                    self.assertCountEqual(actual_keys, expected_keys)
+@parametrize(PASSPORT_TESTS)
+def test_passport(monkeypatch, inputs, expected):
+    params = {k: v[0] for k, v in Passport.get_option_info().items()}
+    class mock(MockSerial):
+        event = threading.Event()
+        data = [b'<123/' + s + b'/something>' for s in inputs]
+    monkeypatch.setattr('plover.machine.base.serial.Serial', mock)
+    actual = []
+    m = Passport(params)
+    m.add_stroke_callback(actual.append)
+    m.start_capture()
+    mock.event.wait()
+    m.stop_capture()
+    assert len(actual) == len(expected)
+    for actual_keys, expected_keys in zip(actual, expected):
+        assert sorted(actual_keys) == sorted(expected_keys)
