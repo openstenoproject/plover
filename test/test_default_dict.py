38c8b088723f9c2735cd666e0bc87dff81da1edc
Benoit Pierre <benoit.pierre@gmail.com>
2018-03-09 13:34:56 +0100

    tests: rework default dictionary test to use pytest

--- test/test_default_dict.py
+++ test/test_default_dict.py
@@ -3,7 +3,6 @@
 
 import io
 import json
-import unittest
 from collections import defaultdict
 
 
@@ -13,25 +12,23 @@ DICT_NAMES = ['main.json',
 DICT_PATH = 'plover/assets/'
 
 
-class TestCase(unittest.TestCase):
-
-    def test_no_duplicates_categorized_files(self):
-        d = defaultdict(list)
-        dictionary = None
-        def read_key_pairs(pairs):
-            for key, value in pairs:
-                d[key].append((value, dictionary))
-            return d
-        for dictionary in map(lambda x: DICT_PATH + x, DICT_NAMES):
-            with io.open(dictionary, encoding='utf-8') as fp:
-                json.load(fp, object_pairs_hook=read_key_pairs)
-        msg_list = []
-        has_duplicate = False
-        for key, value_list in d.items():
-            if len(value_list) > 1:
-                has_duplicate = True
-                msg_list.append('key: %s\n' % key)
-                for value in value_list:
-                    msg_list.append('%r in %s\n' % value)
-        msg = '\n' + ''.join(msg_list)
-        self.assertFalse(has_duplicate, msg)
+def test_no_duplicates_categorized_files():
+    d = defaultdict(list)
+    dictionary = None
+    def read_key_pairs(pairs):
+        for key, value in pairs:
+            d[key].append((value, dictionary))
+        return d
+    for dictionary in map(lambda x: DICT_PATH + x, DICT_NAMES):
+        with io.open(dictionary, encoding='utf-8') as fp:
+            json.load(fp, object_pairs_hook=read_key_pairs)
+    msg_list = []
+    has_duplicate = False
+    for key, value_list in d.items():
+        if len(value_list) > 1:
+            has_duplicate = True
+            msg_list.append('key: %s\n' % key)
+            for value in value_list:
+                msg_list.append('%r in %s\n' % value)
+    msg = '\n' + ''.join(msg_list)
+    assert not has_duplicate, msg
