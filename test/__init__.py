28419f762aa922293f79dcced9088f22f4168074
Benoit Pierre <benoit.pierre@gmail.com>
2018-04-09 06:20:49 +0200

    tests: rework key combo tests to use pytest

--- test/__init__.py
+++ test/__init__.py
@@ -1,32 +1,10 @@
-
 import inspect
-import unittest
 
 import pytest
 
 from plover import system
 from plover.config import DEFAULT_SYSTEM_NAME
 from plover.registry import registry
-
-
-# So our custom assertHelpers are not part of the test failure tracebacks.
-__unittest = True
-
-
-class TestCase(unittest.TestCase):
-
-    def assertRaisesWithMessage(self, exception, msg):
-        outer_self = self
-        class ContextManager:
-            def __enter__(self):
-                pass
-            def __exit__(self, exc_type, exc_value, traceback):
-                if exc_type is exception:
-                    return True
-                if exc_type is None:
-                    outer_self.fail(msg=msg)
-                return False
-        return ContextManager()
 
 
 # Setup registry.
