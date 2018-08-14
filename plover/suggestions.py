c7d9035c63ca28468858a40e20f3fecc8b51391e
fourshade <synnethrelmn@yahoo.com>
2018-08-13 19:00:20 -0500

    Preserve order of suggestions

--- plover/suggestions.py
+++ plover/suggestions.py
@@ -7,14 +7,14 @@ Suggestion = collections.namedtuple('Suggestion', 'text steno_list')
 
 def find_suggestions(dictionary, translation):
     # List of base translations to try looking up, including stripped and case variations.
-    # Make it start out as a set so that duplicates are removed.
-    base = list({
+    # To remove duplicates while preserving list order, run it through an OrderedDict as a key list.
+    base = collections.OrderedDict.fromkeys([
         translation,               # Self
         translation.strip(' '),    # Strip spaces (patterns with \n or \t are correctly handled)
         translation.lower(),       # All lowercase
         translation.capitalize(),  # First letter capitalized
         translation.upper(),       # All uppercase
-    })
+    ]).keys()
 
     # List of modifications to apply to each base to find prefixes, suffixes, commands etc.
     mods = [
@@ -29,8 +29,8 @@ def find_suggestions(dictionary, translation):
         lambda s: '{#%s}' % s,     # Raw keystrokes
     ]
 
-    # Make an iterator to run each non-empty base translation through each mod in turn.
-    possible_translations = (mod(t) for t in base if t for mod in mods)
+    # Run each non-empty base translation through each mod in turn.
+    possible_translations = [mod(t) for t in base if t for mod in mods]
 
     # Look up every possibility and return a suggestion for each one with a non-empty result.
     suggestions = []
