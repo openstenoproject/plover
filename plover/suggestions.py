507e896fdba47ec8380cd3dac3141fa77f83d137
fourshade <synnethrelmn@yahoo.com>
2018-08-31 21:55:58 -0500

    suggestions: rewrite to use new search options

--- plover/suggestions.py
+++ plover/suggestions.py
@@ -1,52 +1,43 @@
 import collections
+import re
 
 from plover.steno import sort_steno_strokes
 
 
 Suggestion = collections.namedtuple('Suggestion', 'text steno_list')
 
+# Hard limit on results returned by search (to avoid slowdown on overly broad searches such as regex .*)
+MATCH_LIMIT = 100
+
 
 class Suggestions:
     def __init__(self, dictionary):
         self.dictionary = dictionary
 
-    def find(self, translation):
+    def find(self, translation, count=MATCH_LIMIT, partial=False, regex=False):
+        """ Find translations that are equal or similar (different case, prefixes, suffixes, etc.) to the given one.
+            Special search types such as partial words and regular expressions are also available.
+            Return a list of suggestion tuples with these translations along with the strokes that can produce them. """
         suggestions = []
-
-        mods = [
-            '%s',  # Same
-            '{^%s}',  # Prefix
-            '{^}%s',
-            '{^%s^}',  # Infix
-            '{^}%s{^}',
-            '{%s^}',  # Suffix
-            '%s{^}',
-            '{&%s}',  # Fingerspell
-            '{#%s}',  # Command
-        ]
-
-        possible_translations = {translation}
-
-        # Only strip spaces, so patterns with \n or \t are correctly handled.
-        stripped_translation = translation.strip(' ')
-        if stripped_translation and stripped_translation != translation:
-            possible_translations.add(stripped_translation)
-
-        lowercase_translation = translation.lower()
-        if lowercase_translation != translation:
-            possible_translations.add(lowercase_translation)
-
-        similar_words = self.dictionary.casereverse_lookup(translation.lower())
-        if similar_words:
-            possible_translations |= set(similar_words)
-
-        for t in possible_translations:
-            for modded_translation in [mod % t for mod in mods]:
-                strokes_list = self.dictionary.reverse_lookup(modded_translation)
-                if not strokes_list:
-                    continue
-                strokes_list = sort_steno_strokes(strokes_list)
-                suggestion = Suggestion(modded_translation, strokes_list)
-                suggestions.append(suggestion)
-
+        # Don't bother looking for suggestions on empty strings or whitespace.
+        if translation and not translation.isspace():
+            # Perform the requested type of lookup. The results should be sorted correctly when received.
+            max_matches = min(count, MATCH_LIMIT)
+            if regex:
+                try:
+                    items = self.dictionary.find_regex(translation, max_matches)
+                except re.error as e:
+                    return [Suggestion("Invalid regular expression", [(e.msg,)])]
+            elif partial:
+                items = self.dictionary.find_partial(translation, max_matches)
+            else:
+                items = self.dictionary.find_similar(translation)[:max_matches]
+            # Package the lookup results into namedtuples for display.
+            for (t, kl) in items:
+                s = Suggestion(t, sort_steno_strokes(kl))
+                # If we got an exact match, put it at the top of the list, otherwise append the results in order.
+                if t == translation:
+                    suggestions.insert(0, s)
+                else:
+                    suggestions.append(s)
         return suggestions
