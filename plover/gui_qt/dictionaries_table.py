3791aa76c0b69b44c457d965cb1f755148343b95
user202729 <25191436+user202729@users.noreply.github.com>
2020-10-16 22:46:01 +0700

    Reword context menu entry name

--- plover/gui_qt/dictionaries_table.py
+++ plover/gui_qt/dictionaries_table.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QWidget, QMenu, QAction, QTableWidget
 class DictionariesTable(QTableWidget):
     def contextMenuEvent(self, event):
         menu = QMenu(self)
-        saveAsAction = QAction("Save as...", self)
+        saveAsAction = QAction("Save a Copy As...", self)
         row = self.rowAt(event.y())
         assert row >= 0
         saveAsAction.triggered.connect(lambda: self.parent().on_save_as(row))
