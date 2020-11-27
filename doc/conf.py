dc28f7f6645480fb88bade743baaede0f924d6b4
Sammi De Guzman <sammi@sammdot.ca>
2020-11-26 16:05:52 -0800

    doc: add ReadTheDocs configuration to documentation directory

---
+++ doc/conf.py
@@ -0,0 +1,47 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+# Add Plover base directory so we can import plover below
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+
+from plover import __name__ as __software_name__, __version__, __copyright__
+
+project = __software_name__.capitalize()
+copyright = "2020 " + __copyright__.replace("(C) ", "")
+author = copyright
+
+release = __version__
+version = release
+
+# -- General configuration ---------------------------------------------------
+
+import sphinx_rtd_theme
+
+extensions = [
+  'sphinx_rtd_theme',
+]
+
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+
+html_static_path = ['_static']
+
+html_sidebars = {
+  '**': [
+    'globaltoc.html',
+    'relations.html',
+    'sourcelink.html',
+    'searchbox.html',
+  ],
+}
---
+++ doc/index.rst
@@ -0,0 +1,14 @@
+Plover |version|\  Documentation
+================================
+
+Plover_ (rhymes with "lover") is a free, open source stenography engine. It
+allows individuals who learn stenography to write into any program at speeds of
+over 200 words per minute. Plover runs on Windows, macOS, and Linux.
+
+Plover is part of the `Open Steno Project`_. The Open Steno Project aims to
+open up stenography to the general public. The Open Steno Project has
+manifested as efforts for free software, free learning resources, and open
+source hardware options.
+
+.. _`Plover`: https://www.openstenoproject.org/plover/
+.. _`Open Steno Project`: https://www.openstenoproject.org/
