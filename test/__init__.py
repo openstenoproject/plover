62e8c6d84cfbb7e724f5b34b88ce48a86ebd55fc
Benoit Pierre <benoit.pierre@gmail.com>
2021-04-27 19:13:45 +0200

    tests: cleaner global setup

--- test/__init__.py
+++
@@ -1,9 +0,0 @@
-from plover import system
-from plover.config import DEFAULT_SYSTEM_NAME
-from plover.registry import registry
-
-
-# Setup registry.
-registry.update()
-# Setup default system.
-system.setup(DEFAULT_SYSTEM_NAME)
---
+++ test/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+from plover import system
+from plover.config import DEFAULT_SYSTEM_NAME
+from plover.registry import registry
+
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_plover():
+    registry.update()
+    system.setup(DEFAULT_SYSTEM_NAME)
