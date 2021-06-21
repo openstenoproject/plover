3af90403b993b7494efee246f77f6da14e22645a
Benoit Pierre <benoit.pierre@gmail.com>
2021-06-21 21:53:40 +0200

    tests: add more `normalize_steno` tests

--- test/test_steno.py
+++ test/test_steno.py
@@ -10,46 +10,54 @@ from plover_build_utils.testing import parametrize
 
 NORMALIZE_TESTS = (
     # TODO: More cases
-    lambda: ('S', 'S'),
-    lambda: ('S-', 'S'),
-    lambda: ('-S', '-S'),
-    lambda: ('ES', 'ES'),
-    lambda: ('-ES', 'ES'),
-    lambda: ('TW-EPBL', 'TWEPBL'),
-    lambda: ('TWEPBL', 'TWEPBL'),
-    lambda: ('19', '1-9'),
-    lambda: ('14', '14'),
-    lambda: ('146', '14-6'),
-    lambda: ('67', '-67'),
-    lambda: ('6', '-6'),
-    lambda: ('9', '-9'),
-    lambda: ('5', '5'),
-    lambda: ('0', '0'),
-    lambda: ('456', '456'),
-    lambda: ('46', '4-6'),
-    lambda: ('4*6', '4*6'),
-    lambda: ('456', '456'),
-    lambda: ('S46', 'S4-6'),
+    lambda: ('S', ('S',)),
+    lambda: ('S-', ('S',)),
+    lambda: ('-S', ('-S',)),
+    lambda: ('ES', ('ES',)),
+    lambda: ('-ES', ('ES',)),
+    lambda: ('TW-EPBL', ('TWEPBL',)),
+    lambda: ('TWEPBL', ('TWEPBL',)),
+    lambda: ('19', ('1-9',)),
+    lambda: ('14', ('14',)),
+    lambda: ('146', ('14-6',)),
+    lambda: ('67', ('-67',)),
+    lambda: ('120-7', ('1207',)),
+    lambda: ('6', ('-6',)),
+    lambda: ('9', ('-9',)),
+    lambda: ('5', ('5',)),
+    lambda: ('0', ('0',)),
+    lambda: ('456', ('456',)),
+    lambda: ('46', ('4-6',)),
+    lambda: ('4*6', ('4*6',)),
+    lambda: ('456', ('456',)),
+    lambda: ('S46', ('S4-6',)),
+    lambda: ('T-EFT/-G', ('TEFT', '-G')),
+    lambda: ('/PRE', ('', 'PRE')),
     # Number key.
-    lambda: ('#S', '#S'),
-    lambda: ('#A', '#A'),
-    lambda: ('#0', '0'),
-    lambda: ('#6', '-6'),
+    lambda: ('#', ('#',)),
+    lambda: ('#S', ('#S',)),
+    lambda: ('#A', ('#A',)),
+    lambda: ('#0', ('0',)),
+    lambda: ('#6', ('-6',)),
     # Implicit hyphens.
-    lambda: ('SA-', 'SA'),
-    lambda: ('SA-R', 'SAR'),
-    lambda: ('-O', 'O'),
-    lambda: ('S*-R', 'S*R'),
-    lambda: ('S-*R', 'S*R'),
+    lambda: ('SA-', ('SA',)),
+    lambda: ('SA-R', ('SAR',)),
+    lambda: ('O', ('O',)),
+    lambda: ('S*-R', ('S*R',)),
+    # Not strictly valid.
+    lambda: ('SRALD/invalid', ('SRALD', 'invalid')),
+    lambda: ('SRALD//invalid', ('SRALD', '', 'invalid')),
+    lambda: ('S-*R', ('S*R',)),
+    lambda: ('-O', ('O',)),
 )
 
 @parametrize(NORMALIZE_TESTS)
-def test_normalize_steno(steno, strokes):
-    result = '/'.join(normalize_steno(steno))
+def test_normalize_steno(steno, expected):
+    result = normalize_steno(steno)
     msg = 'normalize_steno(%r)=%r != %r' % (
-        steno, result, strokes,
+        steno, result, expected,
     )
-    assert result == strokes, msg
+    assert result == expected, msg
 
 
 STROKE_TESTS = (
