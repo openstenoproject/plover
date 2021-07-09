2fa3ae4ac66f98dfc8dacc206f84a6178aa894b3
Sammi De Guzman <sammi@sammdot.ca>
2021-07-08 20:11:03 -0700

    doc: Change theme to customized Furo to better match Plover branding

--- doc/conf.py
+++ doc/conf.py
@@ -23,7 +23,7 @@ version = release
 import sphinx_rtd_theme
 
 extensions = [
-  'sphinx_rtd_theme',
+  'sphinx_inline_tabs',
   'sphinxcontrib.yt',
 ]
 
@@ -31,18 +31,43 @@ templates_path = ['_templates']
 
 exclude_patterns = []
 
+pygments_style = "manni"
+pygments_dark_style = "monokai"
+
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 html_static_path = ['_static']
 
-html_sidebars = {
-  '**': [
-    'globaltoc.html',
-    'relations.html',
-    'sourcelink.html',
-    'searchbox.html',
-  ],
+html_title = f"{project} {version}"
+
+html_css_files = [
+  "custom.css",
+]
+
+html_theme_options = {
+  # "sidebar_hide_name": True,
+  "navigation_with_keys": True,
+  "light_css_variables": {
+    "color-brand-primary": "#3d6961",
+    "color-brand-content": "#3d6961",
+    "color-sidebar-background": "#3d6961",
+    "color-sidebar-brand-text": "white",
+    "color-sidebar-brand-text--hover": "white",
+    "color-sidebar-link-text": "white",
+    "color-sidebar-link-text--top-level": "white",
+    "color-sidebar-item-background--hover": "#71a89f",
+    "color-sidebar-item-expander-background--hover": "#71a89f",
+    "color-inline-code-background": "transparent",
+    "font-stack--header": "'Patua One', -apple-system, BlinkMacSystemFont, 'Segoe UI', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji'",
+    "font-stack--monospace": "'JetBrains Mono', SFMono-Regular, Menlo, Consolas, Monaco, Liberation Mono, Lucida Console, monospace",
+  },
+  "dark_css_variables": {
+    "color-link": "#68a69b",
+    "color-link--hover": "#68a69b",
+  },
+  "light_logo": "dolores.svg",
+  "dark_logo": "dolores.svg",
 }
