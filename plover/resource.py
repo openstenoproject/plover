d02bd9b0afc17a58fc4288cc26970f1d2ca0d3a4
Benoit Pierre <benoit.pierre@gmail.com>
2021-12-16 00:37:11 +0100

    resource: rework to make patching (for tests) easier

--- plover/resource.py
+++ plover/resource.py
@@ -1,4 +1,3 @@
-
 from contextlib import contextmanager
 from tempfile import NamedTemporaryFile
 import os
@@ -9,22 +8,23 @@ import pkg_resources
 
 ASSET_SCHEME = 'asset:'
 
-def _asset_split(resource_name):
+
+def _asset_filename(resource_name):
     components = resource_name[len(ASSET_SCHEME):].split(':', 1)
     if len(components) != 2:
         raise ValueError('invalid asset: %s' % resource_name)
     if os.path.isabs(components[1]):
         raise ValueError(f'invalid asset: {resource_name}')
-    return components
+    return pkg_resources.resource_filename(*components)
 
 def resource_exists(resource_name):
     if resource_name.startswith(ASSET_SCHEME):
-        return pkg_resources.resource_exists(*_asset_split(resource_name))
+        resource_name = _asset_filename(resource_name)
     return os.path.exists(resource_name)
 
 def resource_filename(resource_name):
     if resource_name.startswith(ASSET_SCHEME):
-        return pkg_resources.resource_filename(*_asset_split(resource_name))
+        resource_name = _asset_filename(resource_name)
     return resource_name
 
 def resource_timestamp(resource_name):
