ff2d0f7f532a4ccbf1ea17d1d87da9e89bf95b86
Lynn <lynn@foldr.moe>
2024-02-09 12:57:40 +0100

    Allow setting lookup font

--- plover/gui_qt/lookup_dialog.py
+++ plover/gui_qt/lookup_dialog.py
@@ -1,10 +1,20 @@
 
 from PyQt5.QtCore import QEvent, Qt
+from PyQt5.QtGui import (
+    QCursor,
+    QFont,
+)
+from PyQt5.QtWidgets import (
+    QAction,
+    QFontDialog,
+    QMenu,
+)
 
 from plover import _
 from plover.translation import unescape_translation
 
 from plover.gui_qt.lookup_dialog_ui import Ui_LookupDialog
+from plover.gui_qt.utils import ToolBar
 from plover.gui_qt.tool import Tool
 
 
@@ -26,6 +36,45 @@ class LookupDialog(Tool, Ui_LookupDialog):
         self.pattern.setFocus()
         self.restore_state()
         self.finished.connect(self.save_state)
+        self.layout().addWidget(ToolBar(
+            self.action_SelectFont,
+        ))
+
+        # Font popup menu.
+        self._font_menu = QMenu()
+        # i18n: Widget: “SuggestionsDialog”, “font” menu.
+        self._font_menu_text = QAction(_('&Text'), self._font_menu)
+        # i18n: Widget: “SuggestionsDialog”, “font” menu.
+        self._font_menu_strokes = QAction(_('&Strokes'), self._font_menu)
+        self._font_menu.addActions([self._font_menu_text, self._font_menu_strokes])
+
+    def _get_font(self, name):
+        return getattr(self.suggestions, name)
+
+    def _set_font(self, name, font):
+        setattr(self.suggestions, name, font)
+
+    def _restore_state(self, settings):
+        for name in (
+            'text_font',
+            'strokes_font',
+        ):
+            font_string = settings.value(name)
+            if font_string is None:
+                continue
+            font = QFont()
+            if not font.fromString(font_string):
+                continue
+            self._set_font(name, font)
+
+    def _save_state(self, settings):
+        for name in (
+            'text_font',
+            'strokes_font',
+        ):
+            font = self._get_font(name)
+            font_string = font.toString()
+            settings.setValue(name, font_string)
 
     def eventFilter(self, watched, event):
         if event.type() == QEvent.KeyPress and \
@@ -47,3 +96,18 @@ class LookupDialog(Tool, Ui_LookupDialog):
         if event.type() == QEvent.ActivationChange and self.isActiveWindow():
             self.pattern.setFocus()
             self.pattern.selectAll()
+
+    def on_select_font(self):
+        action = self._font_menu.exec_(QCursor.pos())
+        if action is None:
+            return
+        if action == self._font_menu_text:
+            name = 'text_font'
+            font_options = ()
+        elif action == self._font_menu_strokes:
+            name = 'strokes_font'
+            font_options = (QFontDialog.MonospacedFonts,)
+        font = self._get_font(name)
+        font, ok = QFontDialog.getFont(font, self, '', *font_options)
+        if ok:
+            self._set_font(name, font)
