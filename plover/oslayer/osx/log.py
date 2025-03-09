0b32109db712ad5c8e5c5f9a141fc26f0f602550
Martin Körner <info@mkrnr.com>
2025-03-09 08:21:05 +0100

    Remove printout of notifications when no notification center is available

--- plover/oslayer/osx/log.py
+++ plover/oslayer/osx/log.py
@@ -20,7 +20,7 @@ class NotificationHandler(logging.Handler):
 
         self.notification_center = NSUserNotificationCenter.defaultUserNotificationCenter()
         if self.notification_center is None:
-            print("Warning: No notification center available, printing notifications to log instead")
+            print("no notification center available (e.g. when running from source); not showing notifications")
             self.always_present_delegator = None
         else:
             self.always_present_delegator = AlwaysPresentNSDelegator.alloc().init()
@@ -28,16 +28,17 @@ class NotificationHandler(logging.Handler):
 
     def emit(self, record):
         if self.notification_center is None:
-            print(f"Notification: {record.levelname.title()}: {self.format(record)}")
-        else:
-            # Notification Center has no levels or timeouts.
-            notification = NSUserNotification.alloc().init()
+            # not showing notifications
+            return
 
-            notification.setTitle_(record.levelname.title())
-            notification.setInformativeText_(self.format(record))
+        # Notification Center has no levels or timeouts.
+        notification = NSUserNotification.alloc().init()
 
-            self.notification_center.setDelegate_(self.always_present_delegator)
-            self.notification_center.deliverNotification_(notification)
+        notification.setTitle_(record.levelname.title())
+        notification.setInformativeText_(self.format(record))
+
+        self.notification_center.setDelegate_(self.always_present_delegator)
+        self.notification_center.deliverNotification_(notification)
 
 class AlwaysPresentNSDelegator(NSObject):
     """
