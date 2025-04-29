9ef3fb788c48666101974ac591ad217e7e4569f2
Martin Koerner <info@mkrnr.com>
2025-04-29 07:50:35 +0200

    Replace deprecated remove_expired_responses method in CachedSession

--- plover/plugins_manager/requests.py
+++ plover/plugins_manager/requests.py
@@ -1,7 +1,7 @@
 import os
 
-from requests_futures.sessions import FuturesSession
-from requests_cache import CachedSession
+from requests_futures import sessions
+import requests_cache
 
 from plover.oslayer.config import CONFIG_DIR
 
@@ -9,7 +9,7 @@ from plover.oslayer.config import CONFIG_DIR
 CACHE_NAME = os.path.join(CONFIG_DIR, '.cache', 'plugins')
 
 
-class CachedSession(CachedSession):
+class CachedSession(requests_cache.CachedSession):
 
     def __init__(self):
         dirname = os.path.dirname(CACHE_NAME)
@@ -18,10 +18,10 @@ class CachedSession(CachedSession):
         super().__init__(cache_name=CACHE_NAME,
                          backend='sqlite',
                          expire_after=600)
-        self.remove_expired_responses()
+        self.cache.delete(expired=True)
 
 
-class CachedFuturesSession(FuturesSession):
+class CachedFuturesSession(sessions.FuturesSession):
 
     def __init__(self, session=None):
         if session is None:
